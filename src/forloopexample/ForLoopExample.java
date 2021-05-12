df396a7b3aa88ac86b68f7fb8fec22bd801ef00e
denokenya <dennismuteti25@gmail.com>
2021-05-12 14:29:06 +0300

    first commit

---
+++ src/forloopexample/ForLoopExample.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forloopexample;
+
+/**
+ *
+ * @author Dennis
+ */
+public class ForLoopExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //initialize the loop
+        
+        for(int i =10; i>1; i--){
+            System.out.println("The value of i is " +i);
+            
+        }
+    }
+    
+}
